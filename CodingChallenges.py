2f3c8f7d0f343a4763c0a9d90c0a2539c1b0c35f
JENNIFER JUO <jenjuo@calvisitor-10-105-190-172.calvisitor.1918.berkeley.edu>
2019-03-23 17:34:04 -0700

    Added in all coding challenges

--- CodingChallenges.py
+++ CodingChallenges.py
@@ -36,3 +36,55 @@ def remove_duplicates(num):
         array += num2[i]
     i+= 1
   return(int(num2))
+
+# Problem 3
+def rotate(num):
+  num = str(num)
+
+  big = 0;
+  for i in num:
+    if int(i) > big:
+      big = int(i)
+
+
+  for i in range(big):
+    lastnum = num[-1:]
+    num = num[:-1]
+    num = lastnum + num
+
+return(int(num))
+
+#Problem 4
+def next_fib(num):
+  i = 0;
+  j = 1;
+  if num == 0:
+    return 0
+  elif num == 1:
+    return 1
+  while (j<num):
+    temp = j
+    j = i + j
+    i = temp
+    
+  return j
+
+# Problem 5
+def most_common (inputnum):
+  outs = []
+  zeroArr = []
+  for i in range(10):
+    outs.append(0)
+    zeroArr.append(0)
+
+  for i in str(inputnum):
+    outs[int(i)]+=1
+
+# Problem 6
+def get_coins(num):
+  q = (int) (num / 25)
+  num = num % 25
+  n = (int) (num / 5)
+  num = num % 5
+
+return int(str(q)+str(n)+str(num))
