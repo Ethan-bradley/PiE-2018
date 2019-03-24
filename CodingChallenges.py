1abb983eca09ca974ec7a4dd2ff84c1953f9de97
SirDrahkir <richard.gross20@auhsdschools.org>
2019-03-23 17:15:32 -0700

    Added getCoins

--- CodingChallenges.py
+++ CodingChallenges.py
@@ -14,3 +14,12 @@ def rotate(num):
     num = lastnum + num
 
   return(int(num))
+
+  # Problem 6
+def get_coins(num):
+  q = (int) (num / 25)
+  num = num % 25
+  n = (int) (num / 5)
+  num = num % 5
+
+  return int(str(q)+str(n)+str(num))
