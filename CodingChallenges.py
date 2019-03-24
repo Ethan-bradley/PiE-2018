0906312fb577ee89a5a78e82ff082dd3226df3e3
SirDrahkir <richard.gross20@auhsdschools.org>
2019-03-23 17:06:50 -0700

    Added rotate function

---
+++ CodingChallenges.py
@@ -0,0 +1,16 @@
+# Problem 3
+def rotate(num):
+  num = str(num)
+
+  big = 0;
+  for i in num:
+    if int(i) > big:
+      big = int(i)
+
+
+  for i in range(big):
+    lastnum = num[-1:]
+    num = num[:-1]
+    num = lastnum + num
+
+  return(int(num))
